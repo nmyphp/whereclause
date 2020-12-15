62acb5cba16ffa604463857c51857c6772d7eff6
nmyphp <544409242@qq.com>
2020-12-15 11:38:51 +0800

    Format and remove tmp dir

--- src/main/java/org/ganaga/whereclause/IWhereClause.java
+++ src/main/java/org/ganaga/whereclause/IWhereClause.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public interface IWhereClause {
-	public boolean accepts(Map<String, Object> attributes) throws WhereClauseException;
 
-	public boolean accepts(Map<String, Object> attributes, Map<String, String> aliases) throws WhereClauseException;
+    public boolean accepts(Map<String, Object> attributes) throws WhereClauseException;
 
-	public List<String> getFields() throws WhereClauseException;
+    public boolean accepts(Map<String, Object> attributes, Map<String, String> aliases) throws WhereClauseException;
+
+    public List<String> getFields() throws WhereClauseException;
 }
